5dcc07b67e1e1a185450b27d10a86ed5a65b24f6
MCCristian <cristi_9840@yahoo.com>
2021-04-09 02:35:44 +0300

    I add the buttons for signIn and signUp

--- app/src/main/java/com/example/gitprojecttest/MainActivity.java
+++ app/src/main/java/com/example/gitprojecttest/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.gitprojecttest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSingIn, getBtnSingUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSingIn = findViewById(R.id.bnt_singIn);
+        getBtnSingUp = findViewById(R.id.btn_singUp);
+
     }
 }
