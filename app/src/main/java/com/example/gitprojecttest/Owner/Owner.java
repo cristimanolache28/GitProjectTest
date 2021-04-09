673e8bcc8bc17b003c05bc49414a7baf3aa1c926
MCCristian <cristi_9840@yahoo.com>
2021-04-09 03:55:33 +0300

    Adaugam clasa Owner si o populam

---
+++ app/src/main/java/com/example/gitprojecttest/Owner/Owner.java
@@ -0,0 +1,37 @@
+package com.example.gitprojecttest.Owner;
+
+public class Owner {
+    private String name;
+    private String address;
+    private double budget;
+
+    public Owner(String name, String address, double budget) {
+        this.name = name;
+        this.address = address;
+        this.budget = budget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+}
