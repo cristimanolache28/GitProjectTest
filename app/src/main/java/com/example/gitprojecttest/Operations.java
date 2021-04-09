67221903eb0b8f10f23365fc245d806c661c3e83
MCCristian <cristi_9840@yahoo.com>
2021-04-09 03:20:08 +0300

    Am creat dua metode in clasa Operation (sum, dif)

---
+++ app/src/main/java/com/example/gitprojecttest/Operations.java
@@ -0,0 +1,30 @@
+package com.example.gitprojecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Operations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operations);
+
+        int a = 10;
+        int b = 5;
+
+    }
+
+    public static int sum(int nr1, int nr2)
+    {
+        return nr1 + nr2;
+    }
+
+    public static int dif(int nr1, int nr2)
+    {
+        return nr1 - nr2;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gitprojecttest/SingUp.java
@@ -0,0 +1,19 @@
+package com.example.gitprojecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+
+    }
+
+
+
+}
