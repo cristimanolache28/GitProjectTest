7f68c528f5da670cc36fe9a9e22345a6c919f0ab
MCCristian <cristi_9840@yahoo.com>
2021-04-09 03:01:03 +0300

    Am create intent pentru ambele metode

--- app/src/main/java/com/example/gitprojecttest/MainActivity.java
+++ app/src/main/java/com/example/gitprojecttest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gitprojecttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,8 +36,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openSingUp() {
+        Intent intent = new Intent(MainActivity.this, SingUp.class);
+        startActivity(intent);
     }
 
     private void openSingIn() {
+        Intent intent = new Intent(MainActivity.this, SingIn.class);
+        startActivity(intent);
     }
 }
